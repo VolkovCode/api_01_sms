8151e2be5f834662089c4d89ebeae317e036dfae
Волков Алексей <wolf.alex.1997@mail.ru>
2020-04-24 19:09:46 +0300

    version 1

--- homework.py
+++ homework.py
@@ -1,20 +1,30 @@
 import time
-
 import requests
 from twilio.rest import Client
+
+account_sid = "AC8c6e212a56ae6307a5e2d345c7b3b23d"
+auth_token = "3071d536eccea43fa1036e72c7464b78"
+client = Client(account_sid, auth_token)
 
 
 def get_status(user_id):
     params = {
-        ...
+        "access_token": "f0aa074ac35127898dfe0ba3e99c8c5770111f095c48ceb83d783c25f269236052133fa78afde2fda5b1c",
+        "user_id": user_id,    
+        "v": "5.92",
+        "fields": ["online"]
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    url = 'https://api.vk.com/method/users.get'
+    user_status = requests.post(url, params)
+    return user_status.json()["response"][0]["online"]  # Верните статус пользователя в ВК
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    message = client.messages.create(
+    to="+79158137334", 
+    from_="+12057297134",
+    body= sms_text)
+    return message.sid  # Верните sid отправленного сообщения из Twilio
 
 
 if __name__ == "__main__":
